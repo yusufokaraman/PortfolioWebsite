89b67c561d29fc828786d242cf855af66b99fc99
yusufokaraman <yusufokaraman@gmail.com>
2021-10-27 01:32:25 +0300

    AnaSayfa Girişi Belirlendi ve Projeye Eklendi.

--- PortfolioWebsite/Controllers/BlogController.cs
+++ PortfolioWebsite/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioWebsite.BusinessLayer.Concrete;
+using PortfolioWebsite.DataAccessLayer.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,17 @@ namespace PortfolioWebsite.Controllers
 {
     public class BlogController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         public IActionResult Index()
         {
-            return View();
+            var result = bm.GetBlogsListWithCategory();
+            return View(result);
+        }
+        public IActionResult BlogReadAll(int id)
+        {
+            ViewBag.i = id;
+            var result = bm.GetBlogByID(id);
+            return View(result);
         }
     }
 }
--- PortfolioWebsite/Controllers/HomeController.cs
+++ PortfolioWebsite/Controllers/HomeController.cs
@@ -23,20 +23,6 @@ namespace PortfolioWebsite.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult Test()
-        {
-            return View();
-        }
+       
     }
 }
